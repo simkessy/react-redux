710615fab8badf5412d4f7d070d53ce7c57cbf2a
Kessy Similien <simkdriod@gmail.com>
2019-08-20 16:09:32 -0700

    add reducers and notes

--- playground.js
+++ playground.js
@@ -1,6 +1,11 @@
+// import dependencies
+const { createStore, combineReducers } = require("redux");
+
 console.clear();
 
-// ACTIONS
+/* 
+    ACTIONS 
+*/
 const createPolicy = (name, amount) => {
   return {
     type: "CREATE_POLICY",
@@ -30,8 +35,9 @@ const createClaim = (name, bagOfMoney) => {
   };
 };
 
-// REDUCERS
-
+/* 
+    REDUCERS     
+*/
 // Set oldClaims default to empty array for first call with no data
 const claimsHistory = (oldClaims = [], action) => {
   // We only care about CREATE_CLAIM
@@ -48,7 +54,7 @@ const accounting = (bagOfMoney = 100, action) => {
   if (action.type === "CREATE_CLAIM") {
     return bagOfMoney - action.payload.bagOfMoney;
   } else if (action.type === "CREATE_POLICY") {
-    return bagOfMoney + action.payload.amount``;
+    return bagOfMoney + action.payload.amount;
   } else {
     return bagOfMoney;
   }
@@ -60,10 +66,36 @@ const policies = (listOfPolicies = [], action) => {
   if (action.type === "CREATE_POLICY") {
     return [...listOfPolicies, action.payload.name];
   } else if (action.type === "DELETE_POLICY") {
-    return listOfPolicies.filter((r, index) => r !== action.payload.name);
+    return listOfPolicies.filter(r => r !== action.payload.name);
+  } else {
+    return listOfPolicies;
   }
 };
 
 // Reduce notes
 // never modify the original data
 // always return data
+
+const ourDepartments = combineReducers({
+  accounting,
+  claimsHistory,
+  policies
+});
+
+const store = createStore(ourDepartments);
+
+const action = createPolicy("Alex", 20);
+//
+/* 
+Dispatch funcction is in the store
+Store = our entire redux applications 
+contains references to reducers  and state 
+
+Has dispatch: when we make an action, data is copied and sent to each reducer 
+*/
+
+store.dispatch(action);
+
+// getState gets all data
+store.getState();
+console.log("state:", store.getState());
